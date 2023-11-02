9428d37eeaa25f27217028c8adc41ab94632edf4
Brett Ranieri <brett.ranieri@gmail.com>
2023-11-01 23:34:23 -0600

    Write test
    test output of get_absolute_url()

--- recipes/tests.py
+++ recipes/tests.py
@@ -35,12 +35,17 @@ class RecipeModelTest(TestCase):
 
     def test_cooking_time(self):
         recipe = Recipe.objects.get(id=1)
-        # compare name value to expected result
+        # compare cooking time value to expected result
         self.assertEqual(recipe.cooking_time, 30)
 
     def test_ingredients_help_text(self):
         recipe = Recipe.objects.get(id=1)
-        # get metadata of name field and use to query its label
+        # get metadata of ingredients field and use to query its help text
         ing_help_text = recipe._meta.get_field("ingredients").help_text
-        # comparae value to expected result
-        self.assertEqual(ing_help_text, "Ingredients must be seperated by commas.")
+        # compare value to expected result
+        self.assertEqual(ing_help_text, "Ingredients must be seperated by commas")
+
+    def test_get_absolute_url(self):
+        recipe = Recipe.objects.get(id=1)
+        # compare url value to expected result
+        self.assertEqual(recipe.get_absolute_url(), "/list/1")
